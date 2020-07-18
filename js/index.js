c9424b92fbc86efd783ed45839d8cfe65763c11c
JulietK <julietgisemba000@>
2020-07-18 18:45:13 +0300

    made minor entries to the js file

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function getAkanName(){
+    let yearOfBirth = document.getElementById("year-input").value ;
+    let monthOfBirth = Number (document.getElementById("month-input").value );
+    let dayOfBirth = Number( document.getElementById("day-input").value );
+    }
+
+    let genders = document.getElementByName ("gender");
